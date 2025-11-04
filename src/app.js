5579cc0f982a05d2c1780fcb1fac45ec05b83cd2
Christian Paul Pinto Perez <christian.pinto@globant.com>
2025-11-03 22:05:51 -0500

    actualizacion rutas /cart

--- src/app.js
+++ src/app.js
@@ -136,7 +136,6 @@ app.delete("/api/products/deleted/:prodId", async (req, res) => {
 });
 
 //pestaña /api/carts/
-const cartsJSON = rutaArchivoDinamic("carts.json");
 
 const getCarts = async () => {
   const pathCarts = rutaArchivoDinamic("carts.json");
@@ -146,44 +145,83 @@ const getCarts = async () => {
 
 app.post("/api/carts", async (req, res) => {
   try {
-    const { name, price } = req.body;
-    if (!name || price == null) {
-      return res.status(400).json({ error: "Faltan datos obligatorios" });
-    }
-    const cartItem = {
-      id: uuidv4(),
-      name,
-      price,
-    };
     const carts = await getCarts();
-    carts.push(cartItem);
+
+    const newCart = {
+      id: uuidv4(),
+      products: [],
+    };
+
+    carts.push(newCart);
 
     const pathCarts = rutaArchivoDinamic("carts.json");
     await fs.writeFile(pathCarts, JSON.stringify(carts, null, 2), "utf-8");
 
-    res.status(201).json({ success: true, carts: cartItem });
+    res.status(201).json({ success: true, cart: newCart });
   } catch (error) {
-    console.error("Error al agregar producto al carrito:", error);
+    console.error("Error al crear carrito:", error);
     res.status(500).json({ error: error.message });
   }
 });
 
 app.get("/api/carts/:cartID", async (req, res) => {
   try {
-    const productId = req.params.prodID;
-    const products = await getProducts();
-    const product = products.find((p) => p.id === productId);
-    if (!product) {
-      return res.status(404).json({ error: "Producto no encontrado" });
+    const { cartID } = req.params;
+    const carts = await getCarts();
+    const cart = carts.find((c) => c.id === cartID);
+
+    if (!cart) {
+      return res.status(404).json({ error: "Carrito no encontrado" });
     }
-    res.status(200).json(product);
+
+    res.status(200).json({ success: true, cart });
   } catch (error) {
-    console.error("Error al leer products.json:", error);
+    console.error("Error al encontrar el carrito", error);
     res.status(500).json({ error: error.message });
   }
 });
 
+app.post("/api/carts/:cartID/product/:pid", async (req, res) => {
+  try {
+    const { cartID, pid } = req.params;
 
+    const carts = await getCarts();
+    const cart = carts.find((c) => c.id === cartID);
 
+    if (!cart) {
+      return res.status(404).json({ error: "Carrito no encontrado" });
+    }
+
+    const products = await getProducts();
+    const product = products.find((p) => p.id === pid);
+
+    if (!product) {
+      return res.status(404).json({ error: "Producto no encontrado" });
+    }
+
+    cart.products = cart.products || [];
+
+    const cartItem = cart.products.find((p) => p.id === pid);
+
+    if (cartItem) {
+      cartItem.quantity += 1;
+    } else {
+      cart.products.push({
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        quantity: 1,
+      });
+    }
+
+    const pathCarts = rutaArchivoDinamic("carts.json");
+    await fs.writeFile(pathCarts, JSON.stringify(carts, null, 2), "utf-8");
+
+    res.status(201).json({ success: true, cart });
+  } catch (error) {
+    console.error("Error al agregar producto al carrito:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = app;
